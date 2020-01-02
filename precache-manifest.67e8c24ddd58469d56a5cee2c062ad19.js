c80574ab12fda292eb1c9694ce25f00f915de9fa
Yuriy Bondar <bondar.europe@gmail.com>
2020-01-02 22:03:44 +0200

    Updates

---
+++ precache-manifest.67e8c24ddd58469d56a5cee2c062ad19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a101a84bddd1058f4a23695bde9341c5",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6202a8903b030987ca5c",
+    "url": "/goit-react-hw-01-components/static/css/main.9a442736.chunk.css"
+  },
+  {
+    "revision": "af7c4d6d25fb2f8585da",
+    "url": "/goit-react-hw-01-components/static/js/2.7022ddee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.7022ddee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6202a8903b030987ca5c",
+    "url": "/goit-react-hw-01-components/static/js/main.032ed543.chunk.js"
+  },
+  {
+    "revision": "e72dda2d2fc4e79c1cb5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60f9dd86.js"
+  }
+]);
